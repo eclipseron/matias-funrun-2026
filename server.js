8b4437103304dacd1fb5abcb55771744e06a99ca
PetrusAriaa <petrusariacr25@gmail.com>
2026-07-06 21:49:57 +0700

    prod: setup production

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
-const { createServer } = require('http')
-const { parse } = require('url')
-const next = require('next')
+import { createServer } from 'http'
+import next from 'next'
 
 const port = parseInt(process.env.PORT || '3000', 10)
 const dev = process.env.NODE_ENV !== 'production'
@@ -9,8 +8,7 @@ const handle = app.getRequestHandler()
 
 app.prepare().then(() => {
   createServer((req, res) => {
-    const parsedUrl = parse(req.url, true)
-    handle(req, res, parsedUrl)
+    handle(req, res)
   }).listen(port)
 
   console.log(
