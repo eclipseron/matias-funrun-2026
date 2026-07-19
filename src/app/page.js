b94689531cfcbf5065ac752c80b29dffc53d6f85
kderrylo <derrylzodxyt@gmail.com>
2026-07-19 18:14:51 +0700

    add force dynamic for realtime db chacing

--- src/app/page.js
+++ src/app/page.js
@@ -1,3 +1,5 @@
+export const dynamic = 'force-dynamic';
+
 import Link from 'next/link';
 import { getCountdownTarget, getRegistrationPeriodWithCurrCount } from '@/lib/registrationPeriods';
 import { 
